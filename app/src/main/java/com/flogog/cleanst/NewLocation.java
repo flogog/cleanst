22d8ba399d71a944cc0ca200b4b68728d5463216
Diego Guadarrama <flogog@localhost.localdomain>
2016-10-01 21:32:27 -0400

    Add Final Document

--- app/src/main/java/com/flogog/cleanst/NewLocation.java
+++ app/src/main/java/com/flogog/cleanst/NewLocation.java
@@ -1,10 +1,13 @@
 package com.flogog.cleanst;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.firebase.client.Firebase;
+import com.flogog.cleanst.menu.Settings;
 import com.flogog.cleanst.pojo.Location;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -20,10 +23,22 @@ public class NewLocation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_location);
 
+        Toolbar cleanStActionBar = (Toolbar) findViewById(R.id.toolbar);
+        cleanStActionBar.setSaveFromParentEnabled(true);
+        cleanStActionBar.setNavigationIcon(R.drawable.left);
+        cleanStActionBar.setTitle(R.string.app_name);
+        cleanStActionBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavUtils.navigateUpTo(NewLocation.this, NewLocation.this.getIntent());
+            }
+        });
+
+
         Bundle param        = getIntent().getExtras();
-        table    = (String)param.get("table");
-        lat       = (Double) param.get("lat");
-        lng       = (Double) param.get("lng");
+        table    = (String)param.get(getString(R.string.item_table));
+        lat       = (Double) param.get(getString(R.string.firebase_locations_latitude));
+        lng       = (Double) param.get(getString(R.string.firebase_locations_longitude));
 
         CircleImageView recycle = (CircleImageView) findViewById(R.id.recycle);
         recycle.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +46,7 @@ public class NewLocation extends AppCompatActivity {
             public void onClick(View v) {
                 Location location = new Location();
                 location.setType(getString(R.string.table_locations_recycle_type));
-                location.setLocationId("Recycle Bin");
+                location.setLocationId(getString(R.string.item_recycle_bin));
                 location.setLatitude(lat);
                 location.setLongitude(lng);
                 location.setComment("");
@@ -45,7 +60,7 @@ public class NewLocation extends AppCompatActivity {
             public void onClick(View v) {
                 Location location = new Location();
                 location.setType(getString(R.string.table_locations_waste_type));
-                location.setLocationId("Waste Bin");
+                location.setLocationId(getString(R.string.item_waste));
                 location.setLatitude(lat);
                 location.setLongitude(lng);
                 location.setComment("");
@@ -63,7 +78,6 @@ public class NewLocation extends AppCompatActivity {
         locationsTable.child(getString(R.string.firebase_locations_id)).setValue(loc.getLocationId());
         locationsTable.child(getString(R.string.firebase_locations_longitude)).setValue(loc.getLongitude().toString());
         locationsTable.child(getString(R.string.firebase_locations_type)).setValue(loc.getType());
-        System.out.print("Record inserted "+loc.toString());
     }
 
 }
